89bfb2b261bfd125928f12d23d5faf7484c22215
neiderbtc <neiderbtc@gmail.com>
2021-10-02 18:31:56 -0500

    secrets added 6

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React, { FC } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/pages/Home';
 
+const Contact = () => {
+  return (<div>
+    contact
+  </div>)
+}
+
 const App: FC = () => {
   return (
     <div className="App">
       <Router basename="/">
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </Router>
     </div>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,13 +7,16 @@ import "./../../assets/css/home.css";
 import CallToAction from "../organisms/sections/CallToAction";
 import Footer from "../organisms/sections/Footer";
 import { Helmet } from "react-helmet";
+type Props = {
+    title?: string;
+}
 
-const Home: FC = () => {
+const Home: FC<Props> = ({title}) => {
 
     return (
         <div className="eading-normal tracking-normal text-white gradient">
             <Helmet>
-                <title>Landing - page</title>
+                <title>{title} - Landing - page</title>
                 <meta name="description" content="Landing page create with reactjs Typescript" />
             </Helmet>
             <NavHeader />
